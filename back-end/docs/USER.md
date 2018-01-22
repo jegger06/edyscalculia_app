89840ec7c95b5c81c952b49d12a29675d4789e5f
Jegger <jeggersaren@yahoo.com>
2018-01-22 16:08:30 +0800

    Added the lists, update, delete of user accounts. User docs has been updated.

--- back-end/docs/USER.md
+++ back-end/docs/USER.md
@@ -68,3 +68,116 @@ Succeed to login:
     }
 }
 ```
+
+## List of users
+Type: GET
+
+Protected: Yes
+
+Endpoint: /api/user/lists?type=1&date=1 [type (1 = ASC | 2 = DESC)] [date (1 = DESC | 2 = ASC)]
+
+Note: Type defaults to ASC. Date defaults to DESC. No need to set params if you want the default.
+
+### Response
+Failed to fetch user accounts.
+```json
+{
+    "success": boolean,
+    "message": string
+}
+```
+There are user account fetch in DB
+```json
+{
+    "success": boolean,
+    "accounts": [
+        {
+            "account_id": number,
+            "account_name": string,
+            "account_bday": string,
+            "account_username": string,
+            "account_date": string | date,
+            "type_id": number,
+            "type_description": string
+        }
+    ]
+}
+```
+
+## User Details
+Type: GET
+
+Protected: Yes
+
+Endpoint: /api/user/:account_id
+
+### Response
+
+Failed to fetch details
+```json
+{
+    "success": boolean,
+    "message": string
+}
+```
+
+Details are available
+```json
+{
+    "success": boolean,
+    "details": {
+        "account_id": number,
+        "account_name": string,
+        "account_bday": string | date,
+        "account_username": string,
+        "account_date": string | date,
+        "type_id": number,
+        "type_description": string
+    }
+}
+```
+
+## Update a User Account
+Type: PUT
+
+Protected: Yes
+
+Endpoint: /api/user/:account_id
+
+Note: The `account_password` is optional to be passed. Pass only if the user/admin changed the password.
+
+### Variable
+```json
+{
+	"type_id": number,
+	"account_name": string,
+    "account_bday": date,
+	"account_password": string
+}
+```
+
+### Response 
+```json
+{
+    "success": boolean,
+    "message": string
+}
+```
+
+## Delete a User Account
+Type: DELETE
+
+Protected: Yes
+
+Endpoint: /api/user/:account_id
+
+Note: The user account won't be deleted if there are still chapters, lessons, questions attached to it.
+
+### Response
+```json
+{
+    "success": boolean,
+    "message": string
+}
+```
+
--- back-end/routes/user.route.js
+++ back-end/routes/user.route.js
@@ -122,48 +122,162 @@ router.post('/login', (req, res) => {
 
 router.get('/lists', passport.authenticate('jwt', { session: false }), (req, res) => {
   if (req.user.type_slog === 'admin') {
-    
+    let sql = 'SELECT a.account_id, a.account_name, a.account_bday, a.account_username, a.account_date, a.type_id, t.type_description FROM tbl_account AS a INNER JOIN tbl_type AS t ON a.type_id = t.type_id ORDER BY ';
+    let param_type = req.query.type;
+    let param_date = req.query.date;
+    switch(param_type) {
+      case '1':
+        sql += 'a.type_id ASC, ';
+      break;
+      case '2':
+        sql += 'a.type_id DESC, ';
+      break;
+      default:
+        sql += 'a.type_id ASC, ';
+    }
+    switch(param_date) {
+      case '1':
+        sql += 'a.account_date DESC';
+      break;
+      case '2':
+        sql += 'a.account_date ASC';
+      break;
+      default:
+        sql += 'a.account_date DESC';
+    }
+    db.query(sql, (err, result) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Something wen\'t wrong fetching accounts in DB. Please try again later.'
+        });
+      }
+      return res.json({
+        success: true,
+        accounts: result
+      });
+    })
   } else {
     return res.json({
       success: false,
       message: 'You don\'t have the rights to view the lists of users.'
     })
   }
-
-
-    let sql = 'SELECT * FROM tbl_account';
-    db.query(sql, (err, result) => {
-        if (err) {
-            console.log('Something wen\'t wrong fetching data from database...');
-        }
-        console.log(result);
-        res.json({
-            result
-        });
-    });
 });
 
-// router.get('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-//     let sql = 'SELECT * FROM tbl_account WHERE account_id = ?';
-//     db.query(sql, req.params.id, (err, result) => {
-//       if (err) {
-//           console.log('Error in fetching data...');
-//       }
-//       if (result.length < 1) {
-//           res.json({
-//               success: false,
-//               message: 'The person does not exist...',
-//               data: result.length
-//           });
-//       } else {
-//           res.json({
-//               success: true,
-//               message: 'Here is the details for this person...',
-//               result,
-//               data: result.length
-//           });
-//       }
-//     });
-// });
+router.get('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const id = req.params.id;
+  let sql = 'SELECT a.account_id, a.account_name, a.account_bday, a.account_username, a.account_date, a.type_id, t.type_description FROM tbl_account AS a INNER JOIN tbl_type AS t ON a.type_id = t.type_id WHERE a.account_id = ? LIMIT 1';
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      return res.json({
+        success: false,
+        message: 'Something wen\'t wrong fetching user details. Please try again later.'
+      });
+    }
+
+    return res.json({
+      success: true,
+      details: result[0]
+    });
+  });
+});
+
+// Update account
+router.put('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const id = req.params.id;
+  if (req.user.type_slog === 'admin' || req.user.account_id == id) {
+    let sql = 'SELECT account_name FROM tbl_account WHERE account_id = ? LIMIT 1';
+    db.query(sql, [id], (err, result) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Error in checking if account exists. Please try again later.'
+        });
+      }
+
+      if (!result.length) {
+        return res.json({
+          success: false,
+          message: 'No account fetch. Please don\'t edit the URL.'
+        });
+      } else {
+        const type_id = req.body.type_id;
+        const account_name = req.body.account_name;
+        const account_bday = req.body.account_bday;
+        let sql = 'UPDATE tbl_account SET type_id = ?, account_name = ?, account_bday = ?';
+        let password = '';
+        if (req.body.account_password) {
+          password = req.body.account_password;
+          let salt = bcrypt.genSaltSync(10);
+          password = bcrypt.hashSync(password, salt);
+          sql += ', account_password = "' + password + '" ';
+        } else {
+          sql += ' ';
+        }
+        sql += 'WHERE account_id = ?';
+        db.query(sql, [type_id, account_name, account_bday, id], (err, result) => {
+          if (err) {
+            return res.json({
+              success: false,
+              message: 'Error in updating account. Please try again.'
+            });
+          }
+
+          res.json({
+            success: true,
+            message: 'Account has been updated.'
+          });
+        });
+      }
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'You don\'t have the rights to update this account.'
+    });
+  }
+});
+
+// Delete an account
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  if (req.user.type_slog === 'admin') {
+    const id = req.params.id;
+    const user_id = req.user.account_id;
+    if (user_id == id) {
+      return res.json({
+        success: false,
+        message: 'You can\'t delete your own account.'
+      });
+    } else {
+      let sql = 'DELETE FROM tbl_account WHERE account_id = ?';
+      db.query(sql, [id], (err, result) => {
+        if (err) {
+          return res.json({
+            success: false,
+            message: 'You can\'t delete the account because there are still attached to it. Delete the chapters, lessons, questions attached to this account before deleting this account.'
+          });
+        }
+        
+        if (!result.affectedRows) {
+          return res.json({
+            success: false,
+            message: 'The account does not exist. Please don\'t edit the URL.',
+          });
+        } else {
+          return res.json({
+            success: true,
+            message: 'Account has been deleted.',
+          });
+        }
+      });
+    }
+  } else {
+    res.json({
+      success: false,
+      message: 'You don\'t have the rights to delete an account.'
+    });
+  }
+});
 
 module.exports = router;
