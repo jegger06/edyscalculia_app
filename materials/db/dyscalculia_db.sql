ce770c870b1b06c0d420f96fad5074077f6c03ef
Jegger <jeggersaren@yahoo.com>
2018-01-25 16:03:45 +0800

    Updated the copy of the DB

--- materials/db/dyscalculia_db.sql
+++ materials/db/dyscalculia_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2018 at 09:25 AM
+-- Generation Time: Jan 25, 2018 at 09:03 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -43,10 +43,9 @@ CREATE TABLE `tbl_account` (
 --
 
 INSERT INTO `tbl_account` (`account_id`, `type_id`, `account_name`, `account_bday`, `account_username`, `account_password`, `account_date`) VALUES
-(1, 1, 'John Doe', '2018-01-15', 'jdoe', 'bcrypt datap', '2018-01-11'),
-(2, 2, 'Jane Smith', '2018-01-01', 'jane', 'bcrypt', '2018-01-02'),
+(1, 2, 'John Doe', '2018-01-15', 'jdoe', 'bcrypt datap', '2018-01-11'),
 (4, 1, 'John Rey Baylen', '1995-07-21', 'jrey', '$2a$10$GSib7MV0TloyMUIVVYJPV.wcDQLGajy23ULqfBqAEDNF7PdGhdogG', '2018-01-15'),
-(5, 1, 'Jegger Saren', '1990-06-13', 'jegger06', '$2a$10$1CG80rL//nXwNGFDhM9G6.B4D1kTrbI288E56KBCCYNkYkquXaw/6', '2018-01-17');
+(5, 1, 'Jegger Saren', '1990-06-13', 'jegger06', '$2a$10$G2BPx41cgs.UNv5Y7c2vhenhiaqddZwb8WOqhfaol8hsttRKKj.D6', '2018-01-17');
 
 -- --------------------------------------------------------
 
@@ -57,11 +56,17 @@ INSERT INTO `tbl_account` (`account_id`, `type_id`, `account_name`, `account_bda
 CREATE TABLE `tbl_answer` (
   `answer_id` int(11) NOT NULL,
   `question_id` int(11) NOT NULL,
-  `answer_slog` varchar(255) NOT NULL,
   `answer_choices` text NOT NULL,
   `answer_key` text NOT NULL,
-  `answer_date` date NOT NULL
+  `answer_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_answer`
+--
+
+INSERT INTO `tbl_answer` (`answer_id`, `question_id`, `answer_choices`, `answer_key`, `answer_date`) VALUES
+(1, 5, 'an object | array in here...', 'sample answer', '2018-01-25 10:33:56');
 
 -- --------------------------------------------------------
 
@@ -83,11 +88,9 @@ CREATE TABLE `tbl_chapter` (
 --
 
 INSERT INTO `tbl_chapter` (`chapter_id`, `account_id`, `chapter_slog`, `chapter_text`, `chapter_status`, `chapter_date`) VALUES
-(3, 1, 'the-awesome-chapter1', 'some title here', 1, '2018-01-17'),
-(6, 1, 'sample-chapter', 'sample chapter', 1, '2018-01-16'),
+(3, 1, 'sample-chapter', 'Sample Chapter', 0, '2018-01-25'),
 (8, 4, 'just-a-notebook', 'Just a Notebook', 1, '2018-01-17'),
-(11, 4, 'readers-digest', 'Readers Digest', 0, '2018-01-17'),
-(12, 4, 'readers-digest1', 'Readers Digest', 0, '2018-01-18');
+(11, 4, 'readers-digest', 'Readers Digest', 0, '2018-01-25');
 
 -- --------------------------------------------------------
 
@@ -97,10 +100,20 @@ INSERT INTO `tbl_chapter` (`chapter_id`, `account_id`, `chapter_slog`, `chapter_
 
 CREATE TABLE `tbl_difficulty` (
   `difficulty_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
   `difficulty_slog` varchar(255) NOT NULL,
   `difficulty_text` varchar(255) NOT NULL,
   `difficulty_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_difficulty`
+--
+
+INSERT INTO `tbl_difficulty` (`difficulty_id`, `account_id`, `difficulty_slog`, `difficulty_text`, `difficulty_date`) VALUES
+(1, 4, 'pre-test', 'Pre test', '2018-01-22'),
+(2, 4, 'post-test', 'Post test', '2018-01-22'),
+(3, 5, 'activities', 'Activities', '2018-01-22');
 
 -- --------------------------------------------------------
 
@@ -119,6 +132,14 @@ CREATE TABLE `tbl_lesson` (
   `lesson_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_lesson`
+--
+
+INSERT INTO `tbl_lesson` (`lesson_id`, `account_id`, `chapter_id`, `lesson_title`, `lesson_slog`, `lesson_content`, `lesson_status`, `lesson_date`) VALUES
+(1, 4, 3, 'Basic Arithmetic', 'basic-arithmetic', 'Update The basics...', 0, '2018-01-22'),
+(2, 4, 11, 'Sample Lesson Title', 'sample-lesson-title', 'some lesson content goes in here...', 1, '2018-01-22');
+
 -- --------------------------------------------------------
 
 --
@@ -128,12 +149,74 @@ CREATE TABLE `tbl_lesson` (
 CREATE TABLE `tbl_question` (
   `question_id` int(11) NOT NULL,
   `lesson_id` int(11) NOT NULL,
+  `question_range_id` int(11) NOT NULL DEFAULT '0' COMMENT '0 if the question is for pre-test',
+  `question_type_id` int(11) NOT NULL,
   `account_id` int(11) NOT NULL,
   `difficulty_id` int(11) NOT NULL,
   `question_content` text NOT NULL,
   `question_status` int(11) NOT NULL DEFAULT '0' COMMENT '0 = Inactive | 1 = active	',
-  `question_date` date NOT NULL
+  `question_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_question`
+--
+
+INSERT INTO `tbl_question` (`question_id`, `lesson_id`, `question_range_id`, `question_type_id`, `account_id`, `difficulty_id`, `question_content`, `question_status`, `question_date`) VALUES
+(3, 2, 1, 3, 1, 1, 'asdasd', 0, '2018-01-15 00:00:00'),
+(4, 1, 1, 1, 5, 2, 'asdasd', 0, '2018-01-15 00:00:00'),
+(5, 1, 1, 3, 5, 2, 'Some question description in here...', 0, '2018-01-25 10:33:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_question_range`
+--
+
+CREATE TABLE `tbl_question_range` (
+  `question_range_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `question_range_slog` varchar(255) NOT NULL,
+  `question_range_from` varchar(255) NOT NULL,
+  `question_range_to` varchar(255) NOT NULL,
+  `question_range_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_question_range`
+--
+
+INSERT INTO `tbl_question_range` (`question_range_id`, `account_id`, `question_range_slog`, `question_range_from`, `question_range_to`, `question_range_date`) VALUES
+(1, 1, '0-20%', '0%', '20%', '2018-01-24 12:53:15'),
+(2, 5, '21%-40%', '21%', '40%', '2018-01-24 13:33:26'),
+(3, 5, '41%-60%', '41%', '60%', '2018-01-24 13:34:19'),
+(4, 5, '61%-80%', '61%', '80%', '2018-01-24 15:39:29'),
+(5, 5, '81%-100%', '81%', '100%', '2018-01-25 15:17:31'),
+(6, 5, '0', '0', '0', '2018-01-25 15:17:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_question_type`
+--
+
+CREATE TABLE `tbl_question_type` (
+  `question_type_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `question_type_slog` varchar(255) NOT NULL,
+  `question_type_text` varchar(255) NOT NULL,
+  `question_type_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_question_type`
+--
+
+INSERT INTO `tbl_question_type` (`question_type_id`, `account_id`, `question_type_slog`, `question_type_text`, `question_type_date`) VALUES
+(1, 5, 'identification', 'Identification', '2018-01-24 14:13:56'),
+(3, 5, 'multiple-choice', 'Multiple Choice', '2018-01-24 14:35:58'),
+(5, 5, 'drag-and-drop1', 'Drag and Drop', '2018-01-24 14:43:02'),
+(6, 5, 'true-or-false', 'true or false', '2018-01-25 09:26:22');
 
 -- --------------------------------------------------------
 
@@ -145,7 +228,7 @@ CREATE TABLE `tbl_type` (
   `type_id` int(11) NOT NULL,
   `type_slog` varchar(255) NOT NULL,
   `type_description` varchar(255) NOT NULL,
-  `type_active` int(11) NOT NULL,
+  `type_status` int(11) NOT NULL DEFAULT '0' COMMENT '0 = inactive | 1 = active	',
   `type_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -153,9 +236,11 @@ CREATE TABLE `tbl_type` (
 -- Dumping data for table `tbl_type`
 --
 
-INSERT INTO `tbl_type` (`type_id`, `type_slog`, `type_description`, `type_active`, `type_date`) VALUES
+INSERT INTO `tbl_type` (`type_id`, `type_slog`, `type_description`, `type_status`, `type_date`) VALUES
 (1, 'admin', 'Admin', 1, '2018-01-11 14:10:17'),
-(2, 'user', 'User', 1, '2018-01-11 14:10:17');
+(2, 'user', 'User', 1, '2018-01-11 14:10:17'),
+(4, 'author', 'Author', 0, '2018-01-23 14:18:15'),
+(5, 'moderator', 'Moderator', 0, '2018-01-23 15:55:09');
 
 --
 -- Indexes for dumped tables
@@ -165,7 +250,8 @@ INSERT INTO `tbl_type` (`type_id`, `type_slog`, `type_description`, `type_active
 -- Indexes for table `tbl_account`
 --
 ALTER TABLE `tbl_account`
-  ADD PRIMARY KEY (`account_id`);
+  ADD PRIMARY KEY (`account_id`),
+  ADD KEY `type_id` (`type_id`);
 
 --
 -- Indexes for table `tbl_answer`
@@ -178,19 +264,23 @@ ALTER TABLE `tbl_answer`
 -- Indexes for table `tbl_chapter`
 --
 ALTER TABLE `tbl_chapter`
-  ADD PRIMARY KEY (`chapter_id`);
+  ADD PRIMARY KEY (`chapter_id`),
+  ADD KEY `account_id` (`account_id`);
 
 --
 -- Indexes for table `tbl_difficulty`
 --
 ALTER TABLE `tbl_difficulty`
-  ADD PRIMARY KEY (`difficulty_id`);
+  ADD PRIMARY KEY (`difficulty_id`),
+  ADD KEY `account_id` (`account_id`);
 
 --
 -- Indexes for table `tbl_lesson`
 --
 ALTER TABLE `tbl_lesson`
-  ADD PRIMARY KEY (`lesson_id`);
+  ADD PRIMARY KEY (`lesson_id`),
+  ADD KEY `account_id` (`account_id`),
+  ADD KEY `chapter_id` (`chapter_id`);
 
 --
 -- Indexes for table `tbl_question`
@@ -198,7 +288,24 @@ ALTER TABLE `tbl_lesson`
 ALTER TABLE `tbl_question`
   ADD PRIMARY KEY (`question_id`),
   ADD KEY `lesson_id` (`lesson_id`),
-  ADD KEY `difficulty_id` (`difficulty_id`);
+  ADD KEY `difficulty_id` (`difficulty_id`),
+  ADD KEY `question_range_id` (`question_range_id`),
+  ADD KEY `tbl_question_ibfk_4` (`question_type_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `tbl_question_range`
+--
+ALTER TABLE `tbl_question_range`
+  ADD PRIMARY KEY (`question_range_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `tbl_question_type`
+--
+ALTER TABLE `tbl_question_type`
+  ADD PRIMARY KEY (`question_type_id`),
+  ADD KEY `account_id` (`account_id`);
 
 --
 -- Indexes for table `tbl_type`
@@ -220,7 +327,7 @@ ALTER TABLE `tbl_account`
 -- AUTO_INCREMENT for table `tbl_answer`
 --
 ALTER TABLE `tbl_answer`
-  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_chapter`
@@ -232,13 +339,13 @@ ALTER TABLE `tbl_chapter`
 -- AUTO_INCREMENT for table `tbl_difficulty`
 --
 ALTER TABLE `tbl_difficulty`
-  MODIFY `difficulty_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `difficulty_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_lesson`
 --
 ALTER TABLE `tbl_lesson`
-  MODIFY `lesson_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `lesson_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_question`
@@ -247,14 +354,32 @@ ALTER TABLE `tbl_question`
   MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `tbl_question_range`
+--
+ALTER TABLE `tbl_question_range`
+  MODIFY `question_range_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_question_type`
+--
+ALTER TABLE `tbl_question_type`
+  MODIFY `question_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `tbl_type`
 --
 ALTER TABLE `tbl_type`
-  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `tbl_account`
+--
+ALTER TABLE `tbl_account`
+  ADD CONSTRAINT `tbl_account_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `tbl_type` (`type_id`) ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `tbl_answer`
@@ -263,11 +388,45 @@ ALTER TABLE `tbl_answer`
   ADD CONSTRAINT `tbl_answer_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `tbl_question` (`question_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
+-- Constraints for table `tbl_chapter`
+--
+ALTER TABLE `tbl_chapter`
+  ADD CONSTRAINT `tbl_chapter_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `tbl_account` (`account_id`) ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tbl_difficulty`
+--
+ALTER TABLE `tbl_difficulty`
+  ADD CONSTRAINT `tbl_difficulty_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `tbl_account` (`account_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tbl_lesson`
+--
+ALTER TABLE `tbl_lesson`
+  ADD CONSTRAINT `tbl_lesson_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `tbl_account` (`account_id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `tbl_lesson_ibfk_2` FOREIGN KEY (`chapter_id`) REFERENCES `tbl_chapter` (`chapter_id`) ON UPDATE NO ACTION;
+
+--
 -- Constraints for table `tbl_question`
 --
 ALTER TABLE `tbl_question`
   ADD CONSTRAINT `tbl_question_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `tbl_lesson` (`lesson_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `tbl_question_ibfk_2` FOREIGN KEY (`difficulty_id`) REFERENCES `tbl_difficulty` (`difficulty_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `tbl_question_ibfk_2` FOREIGN KEY (`difficulty_id`) REFERENCES `tbl_difficulty` (`difficulty_id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `tbl_question_ibfk_4` FOREIGN KEY (`question_type_id`) REFERENCES `tbl_question_type` (`question_type_id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `tbl_question_ibfk_5` FOREIGN KEY (`question_range_id`) REFERENCES `tbl_question_range` (`question_range_id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `tbl_question_ibfk_6` FOREIGN KEY (`account_id`) REFERENCES `tbl_account` (`account_id`) ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tbl_question_range`
+--
+ALTER TABLE `tbl_question_range`
+  ADD CONSTRAINT `tbl_question_range_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `tbl_account` (`account_id`) ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tbl_question_type`
+--
+ALTER TABLE `tbl_question_type`
+  ADD CONSTRAINT `tbl_question_type_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `tbl_account` (`account_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
