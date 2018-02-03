fbedc54ee40210d2ebefeb434ae0c53a8e61f0b9
John Rey M. Baylen <jreywebdeveloper211995@gmail.com>
2018-02-04 00:09:03 +0800

    Done with API call and code cleanup

--- front-end/src/pages/admin-chapter-lesson-questions/admin-chapter-lesson-questions.ts
+++ front-end/src/pages/admin-chapter-lesson-questions/admin-chapter-lesson-questions.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the AdminChapterLessonQuestionsPage page.
@@ -8,6 +9,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+import { api } from './../../config/index';
 
 @IonicPage()
 @Component({
@@ -19,15 +21,46 @@ export class AdminChapterLessonQuestionsPage {
   user: Object = {};
   lesson: Object = {};
   sort: string | number = 2;
-  questions: Object = {};
+  questionsList: Array<{
+    question_id: number,
+    question_range_id: number,
+    question_type_id: number,
+    account_id: number,
+    difficulty_id: number,
+    question_content: string,
+    question_status: number,
+    question_date: string,
+    account_name: string,
+    answer_id: number,
+    answer_choices: string,
+    answer_key: string
+  }>;
+  questionsListCount: number = 0;
+  questionTitle: string = 'Adding';
 
   constructor (
     public navCtrl: NavController,
     public storage: Storage,
+    public http: HttpClient,
+    public toastCtrl: ToastController,
+    public alertCtrl: AlertController,
     public navParams: NavParams) { }
 
-  fetchQuestions (sort: string | number) {
-    this.questions['questionsCount'] = 0;
+  updateQuestion (question: Object): void {
+    console.log(question)
+  }
+
+  deleteQuestion (id: number): void {
+    console.log(id)
+  }
+
+  fetchLessonQuestions (sort: string | number): void {
+    this.http.get(`${ api.host }/question/lists/${ this.lesson['lesson_id'] }`).subscribe(response => {
+      if (response['success']) {
+        this.questionsList = response['questions'];
+        this.questionsListCount = response['questions']['length'];
+      }
+    })
   }
 
   ionViewDidLeave (): void {
@@ -41,7 +74,7 @@ export class AdminChapterLessonQuestionsPage {
         return;
       }
       this.user = response;
-      this.storage.get('chapter-lesson').then(lesson => (this.lesson = lesson) && this.fetchQuestions(this.sort));
+      this.storage.get('chapter-lesson').then(lesson => (this.lesson = lesson) && this.fetchLessonQuestions(this.sort));
     });
   }
 
